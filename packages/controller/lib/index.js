4c0ddc7ea0d3bec5074d6f5980b48db50aa62386
proxy-alt <proxy-alt@proxy-alt.dev>
2026-01-07 16:31:42 -0500

    I'm so stupid x2

--- packages/controller/lib/index.js
+++ packages/controller/lib/index.js
@@ -1,4 +1,7 @@
-import { resolve } from 'node:path'
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const path = resolve(__dirname, "..", "dist");
 
