c7de86cff0fa2dddabd3111464b51ef533452992
Proxy <proxy-alt@proxy-alt.dev>
2026-01-29 21:31:22 -0500

    Up wasm-bindgen

--- packages/core/rewriter/wasm/build.sh
+++ packages/core/rewriter/wasm/build.sh
@@ -27,7 +27,7 @@ which cargo wasm-bindgen wasm-opt wasm-snip &> /dev/null || {
 	exit 1
 }
 
-WBG="wasm-bindgen 0.2.105"
+WBG="wasm-bindgen 0.2.108"
 if ! [[ "$(wasm-bindgen -V)" =~ ^"$WBG" ]]; then
 	echo "Incorrect wasm-bindgen-cli version: '$(wasm-bindgen -V)' != '$WBG'"
 	exit 1
